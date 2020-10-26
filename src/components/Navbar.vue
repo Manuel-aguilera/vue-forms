e6355dabe388d2ad3b4ca22fc68352ad06c2a2f9
Manuel Aguilera <manuel_aguilerita@hotmail.com>
2020-10-26 14:19:42 -0600

    1.0 Formulario con bootstrap

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="navbar navbar-dark bg-dark">
+      <router-link to="/" class="navbar-brand">
+        App
+      </router-link>
+      <div class="d-flex">
+          <router-link to="/" class="btn Ebtn-dark">Tareas</router-link>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,74 @@
 <template>
-  <div class="home">
-    
-    Hello
-  </div>
+  <form >
+    <input type="text"
+    class="form-control mt-2"
+    placeholder="Nombre"
+    v-model="tarea.nombre">
+
+    <div class="my-2">
+      <div class="form-group form-check-inline">
+        <input type="checkbox" class="form-check-input" id="check-1" value="JavaScript" v-model="tarea.categories">
+        <label class="form-check-label" for="check-1">JavaScript</label>
+      </div>
+      <div class="form-group form-check-inline">
+        <input type="checkbox" class="form-check-input" id="check-2" value="Desarrollo" v-model="tarea.categories">
+        <label class="form-check-label" for="check-2">Desarrollo Web</label>
+      </div>
+    </div>
+
+    <div class="my-2">
+      <div class="form-check form-check-inline">
+        <input 
+          class="form-check-input" 
+          type="radio" 
+          id="inlineRadio1" 
+          value="urgente"
+          v-model="tarea.estado"
+        >
+        <label class="form-check-label" for="inlineRadio1">
+          Urgente
+        </label>
+      </div>
+
+      <div class="form-check form-check-inline">
+        <input 
+          class="form-check-input" 
+          type="radio" 
+          id="inlineRadio2" 
+          value="relax"
+          v-model="tarea.estado"
+        >
+        <label class="form-check-label" for="inlineRadio2">
+          Relax
+        </label>
+      </div>
+    </div>
+
+    <div class="my-2">
+      <input 
+          class="form-control" 
+          type="Number"  
+          placeholder="Número"
+          v-model.number="tarea.numero"
+        >
+    </div>}
+
+    <input 
+      type="text" 
+      placeholder="Nombre Tarea"
+      class="form-control"
+      v-model.trim="tarea.nombre"
+    >
+
+    <button 
+      class="btn btn-block btn-dark" 
+      type="submit"
+    >
+      Enviar
+    </button>
+
+  </form>
+  <p>{{tarea}}</p>
 </template>
 
 <script>
@@ -12,6 +78,19 @@ export default {
   name: 'Home',
   components: {
     
+  },
+  data() {
+    return {
+      tarea: {nombre: '', categories: [], estado: '', numero: 0},
+    }
+  },
+  methods: {
+    procesarFormulario() {
+      if(this.tarea.nombre.trim() === ''){
+        console.log('Nombre vacío');
+        return;
+      }
+    }
   }
 }
 </script>
